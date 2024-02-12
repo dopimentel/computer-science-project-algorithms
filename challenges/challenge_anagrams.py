22dab8b5f642382ddb0749f53efde17c8d4be35d
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-12 18:27:47 -0300

    feat: merge_sort_string

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,45 @@
+def merge_sort(string):
+    """Ordena a string usando o algoritmo merge sort."""
+    if isinstance(string, str):
+        string = list(string)
+    if len(string) > 1:
+        middle = len(string) // 2
+        left_half = string[:middle]
+        right_half = string[middle:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        merge(string, left_half, right_half)
+    return "".join(string)
+
+
+def merge(string, left_half, right_half):
+    i = j = k = 0
+
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
+            string[k] = left_half[i]
+            i += 1
+        else:
+            string[k] = right_half[j]
+            j += 1
+        k += 1
+
+    while i < len(left_half):
+        string[k] = left_half[i]
+        i += 1
+        k += 1
+
+    while j < len(right_half):
+        string[k] = right_half[j]
+        j += 1
+        k += 1
+
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
     raise NotImplementedError
+
+
+print(merge_sort("amor"))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,3 +7,11 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word[low_index] != word[high_index]:
         return False
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+if __name__ == "__main__":
+    print(is_palindrome_recursive("ANA", 0, 2))
+    print(is_palindrome_recursive("AGUA", 0, 3))
+    print(is_palindrome_recursive("SOCOS", 0, 4))
+    print(is_palindrome_recursive("REVIVER", 0, 6))
+    print(is_palindrome_recursive("I", 0, 0))
