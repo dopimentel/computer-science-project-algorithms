846ebc339b15b5be51cd50aaaa1bd80504efdea4
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-12 19:21:05 -0300

    feat: req5 max complexity fixed O(n log n)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,12 +1,18 @@
+def validate_input(num):
+    if not isinstance(num, int):
+        return False
+    if num < 0:
+        return False
+    return True
+
+
 def find_duplicate(nums):
     if not nums:
         return False
 
     nums.sort()
     for i in range(len(nums) - 1):
-        if not isinstance(nums[i], int):
-            return False
-        if nums[i] < 0:
+        if not validate_input(nums[i]):
             return False
         if nums[i] == nums[i + 1]:
             return nums[i]
