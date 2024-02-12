4e78080c06275fe6879100ea5fd6a4e5572db5e8
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-12 19:12:53 -0300

    feat: req5 partial

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,5 +1,13 @@
 def find_duplicate(nums):
+    if not nums:
+        return False
+
     nums.sort()
     for i in range(len(nums) - 1):
+        if not isinstance(nums[i], int):
+            return False
+        if nums[i] < 0:
+            return False
         if nums[i] == nums[i + 1]:
             return nums[i]
+    return False
