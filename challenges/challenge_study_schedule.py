03766a48cfd00e5b1c9952a00696be2f869a935d
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-12 15:24:32 -0300

    req1 - brute-force algorithm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,49 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def validate_tuple(tup):
+    return (
+        isinstance(tup, tuple)
+        and len(tup) == 2
+        and all(isinstance(i, int) for i in tup)
+    )
+
+
+def study_schedule(permanence_period, target_time=None):
+    if target_time is None:
+        return None
+    count = 0
+    for period in permanence_period:
+        if not validate_tuple(period):
+            return None
+
+        if target_time in range(period[0], period[1] + 1):
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    print(
+        study_schedule(
+            [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)], 5
+        )
+    )
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1))
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)], 3))
+    print(study_schedule([(1, 1), (2, 2), (3, 3), (4, 4)], 1))
+    print(study_schedule([(1, 2), (1, 3), (2, 3)], 2))
+    print(
+        study_schedule(
+            [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)], None
+        )
+    )
+    print(
+        study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], None)
+    )
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)], None))
+    print(study_schedule([(1, 1), (2, 2), (3, 3), (4, 4)], None))
+    print(study_schedule([(1, 2), (1, 3), (2, 3)], None))
+    print(
+        study_schedule(
+            [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)], 5
+        )
+    )
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1))
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)], 3))
