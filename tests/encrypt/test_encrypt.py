8064f8eb0c2f1f72c8e98eaacdfa19f0db0085b3
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-12 16:36:45 -0300

    test encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    """Test the encrypt_message function."""
+
+    with pytest.raises(TypeError):
+        encrypt_message(1, 1)
+    assert encrypt_message("axdefg", 1) == "a_gfedx"
+    assert encrypt_message("axdefg", 3) == "dxa_gfe"
+
+    assert encrypt_message("axdefg", 2) == "gfed_xa"
+    assert encrypt_message("axdefg", 4) == "gf_edxa"
+
+    assert encrypt_message("axdefg", 9) == "gfedxa"
+    assert encrypt_message("axdefg", 6) == "gfedxa"
